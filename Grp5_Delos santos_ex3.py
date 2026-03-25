98b6b231c7e234b5e7df3a6dbf220ef34b14b612
esdelossantos-hub <esdelossantos@paterostechnologicalcollege.edu.ph>
2026-03-25 11:32:55 +0800

    Create Grp5_Delos santos_ex3.py

---
+++ Grp5_Delos santos_ex3.py
@@ -0,0 +1,28 @@
+# My first simple script
+
+# Print introduction
+print("Hello, I'm ERICKA DELOS SANTOS! From 1V")
+
+# Python Variables
+number = "1V"
+name = "eca"
+
+print(number)
+print(name)
+
+# Python - Variable Names (first names of group members, not including yours)
+rian = "Rian"
+mark = "Mark"
+jc = "Jc"
+micah = "Micah"
+nash = "Nash"
+
+print(rian, mark, jc, micah, nash)
+
+# Many Values to Multiple Variables
+x, y, z = 1, 2, 3
+print(x, y, z)
+
+# One Value to Multiple Variables
+a = b = c = "Group5"
+print(a, b, c)
